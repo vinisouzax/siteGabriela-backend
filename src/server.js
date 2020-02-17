18d54b2477dfd5ff9c13ccb2d482634068bf4b50
viniciusgoncalves2019 <viniciusgoncalves2019@usp.br>
2020-02-17 19:25:20 -0300

    duas urls válidas

--- src/server.js
+++ src/server.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO_URL, {
 //req.query = Acessar query params (para filtros)
 //req.params = Acessar route params (para edição, delete)
 //req.body = Acessar corpo da requisição (para criação, edição)
-app.use(cors({ origin: process.env.APP_URL }, { origin: process.env.APP_URL2 }));
+app.use(cors({ origin: process.env.APP_URL, origin: process.env.APP_URL2 }));
 app.use(express.json())
 app.use('/files', express.static(path.resolve(__dirname, '../', 'uploads')));
 app.use(routes);
