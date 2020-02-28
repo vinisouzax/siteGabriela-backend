76b66570005a3eef558699d8c8fdf657c8fb4393
goncalvesRanor <engenharia.goncalves@ranor.com.br>
2020-02-28 11:40:00 -0300

    wokeDyno BACK_URL

--- src/wokeDyno.js
+++ src/wokeDyno.js
@@ -2,36 +2,49 @@ const fetch = require("node-fetch");
 
 const wakeUpDyno = (url, interval = 28, callback) => {
     let milliseconds = interval * 60000;
-    
-    setTimeout(() => {
 
-        try { 
-            console.log(`Acordando Dyno!`);
-            
-            fetch(url).then(() => console.log(`Buscando ${url}.`)); 
-        }
-        catch (err) { 
-            console.log(`Erro na busca ${url}: ${err.message} 
-            Tentando novamente em ${interval} minutos...`);
-        }
-        finally {
 
-            try {
-                callback(); // execute callback, if passed
+    try { 
+        console.log(`Acordando Dyno!`);
+        
+        fetch(process.env.BACK_URL).then(() => 
+        console.log(`Buscando ${process.env.BACK_URL}.`)); 
+    }
+    catch (err) { 
+        console.log(`Erro na busca ${url}: ${err.message} 
+        Tentando novamente em ${interval} minutos...`);
+    }
+    finally {
+        setTimeout(() => {
+
+            try { 
+                console.log(`Acordando Dyno!`);
+                
+                fetch(url).then(() => console.log(`Buscando ${url}.`)); 
             }
-            catch (e) { // catch callback error
-                callback ? console.log("Chamada falhou: ", e.message) : null;
+            catch (err) { 
+                console.log(`Erro na busca ${url}: ${err.message} 
+                Tentando novamente em ${interval} minutos...`);
             }
             finally {
-                // do it all again
-                const hours = new Date().getHours();
-                interval = (hours > '08' && hours < '23' || hours == '00') ? 28 : 480;
-                return wakeUpDyno(url, interval, callback);
+    
+                try {
+                    callback(); // execute callback, if passed
+                }
+                catch (e) { // catch callback error
+                    callback ? console.log("Chamada falhou: ", e.message) : null;
+                }
+                finally {
+                    // do it all again
+                    const hours = new Date().getHours();
+                    interval = (hours > '08' && hours < '23' || hours == '00') ? 28 : 480;
+                    return wakeUpDyno(url, interval, callback);
+                }
+                
             }
-            
-        }
-
-    }, milliseconds);
+    
+        }, milliseconds);
+    }    
 };
 
 module.exports = wakeUpDyno;
