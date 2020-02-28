7da70ce447a863d8715043cbb76c88d025606caa
viniciusgoncalves2019 <viniciusgoncalves2019@usp.br>
2020-02-28 19:28:15 -0300

    Período do dyno de 12 a 00

--- src/wokeDyno.js
+++ src/wokeDyno.js
@@ -37,7 +37,7 @@ const wakeUpDyno = (url, interval = 28, callback) => {
                 finally {
                     // do it all again
                     const hours = new Date().getHours();
-                    interval = (hours > '09' && hours < '21') ? 28 : 540;
+                    interval = (hours > '12' && hours < '23' || hours < '00') ? 28 : 540;
                     return wakeUpDyno(url, interval, callback);
                 }
                 
